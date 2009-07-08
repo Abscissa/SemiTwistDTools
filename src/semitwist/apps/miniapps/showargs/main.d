d40ee1885ee40316fe5952e03280a8ffb79b042a
Abscissa256 <Abscissa256>
2009-07-08 08:35:53 +0000

    Part of multipart commit
    git-svn-id: file:///home/nick/dev/d/proj/svn-to-git-scrap/svn-dump/filtered-repos/SemiTwistDTools/trunk@67 183b819f-4d41-4c81-9374-59e59addc796

---
+++ src/semitwist/apps/miniapps/showargs/main.d
@@ -0,0 +1,23 @@
+// SemiTwist D Tools
+// MiniApps: Show Args
+// Written in the D programming language.
+
+/** 
+Author:
+$(WEB www.semitwist.com, Nick Sabalausky)
+
+Uses:
+- DMD 1.043
+- Tango 0.99.8
+*/
+
+module miniapps.showargs.main;
+
+import tango.io.Stdout;
+
+void main(char[][] args)
+{
+	Stdout.formatln("args.length: {}", args.length);
+	foreach(int i, char[] arg; args)
+		Stdout.formatln("args[{}]: {}", i, arg);
+}
