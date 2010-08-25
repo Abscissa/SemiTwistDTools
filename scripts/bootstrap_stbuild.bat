ea6df7f83c639322affd075fe517afd4cabaa279
Abscissa256 <Abscissa256>
2010-08-25 03:05:48 +0000

    (NB) all: Broke bootsrap scripts in previous commit.
    git-svn-id: file:///home/nick/dev/d/proj/svn-to-git-scrap/svn-dump/filtered-repos/SemiTwistDTools/trunk@140 183b819f-4d41-4c81-9374-59e59addc796

--- scripts/bootstrap_stbuild.bat
+++ scripts/bootstrap_stbuild.bat
@@ -6,7 +6,7 @@ del /Q _junk_.junk
 del /Q _junk_.junk2
 del /Q obj\*.*
 
-rdmd rdmdAlt.d src\semitwist\apps\stmanage\stbuild\main.d -ofbin\stbuild_bootstrap -odobj -Isrc -release -O
+rdmd rdmdAlt.d -ofbin\stbuild_bootstrap -odobj -Isrc -release -O src\semitwist\apps\stmanage\stbuild\main.d
 
 move *.map obj > _junk_.junk 2> _junk_.junk2
 del /Q _junk_.junk
