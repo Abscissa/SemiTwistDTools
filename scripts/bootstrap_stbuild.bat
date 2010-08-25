288d115df3699ca11228c237d2e0c0b1633c1d7f
Abscissa256 <Abscissa256>
2010-08-25 03:44:57 +0000

    (NB) bootstrap: Worked around a weird BATCH race condition on Windows.
    git-svn-id: file:///home/nick/dev/d/proj/svn-to-git-scrap/svn-dump/filtered-repos/SemiTwistDTools/trunk@142 183b819f-4d41-4c81-9374-59e59addc796

--- scripts/bootstrap_stbuild.bat
+++ scripts/bootstrap_stbuild.bat
@@ -6,7 +6,9 @@ del /Q _junk_.junk
 del /Q _junk_.junk2
 del /Q obj\*.*
 
-rdmd rdmdAlt.d --build-only -ofbin\stbuild_bootstrap -odobj -Isrc -release -O src\semitwist\apps\stmanage\stbuild\main.d
+REM Pre-compiling rdmdAlt seems to be needed to prevent a BATCH race condition
+rdmd --build-only -ofrdmdAlt rdmdAlt.d
+rdmdAlt --build-only -ofbin\stbuild_bootstrap -odobj -Isrc -release -O src\semitwist\apps\stmanage\stbuild\main.d
 
 move *.map obj > _junk_.junk 2> _junk_.junk2
 del /Q _junk_.junk
