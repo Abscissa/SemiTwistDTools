cc57e47713009cf842bf28dbe042ed7fdec206ac
Abscissa256 <Abscissa256>
2010-08-25 05:56:58 +0000

    (NB) stbuild: Cleaned out old comments.
    git-svn-id: file:///home/nick/dev/d/proj/svn-to-git-scrap/svn-dump/filtered-repos/SemiTwistDTools/trunk@145 183b819f-4d41-4c81-9374-59e59addc796

--- src/semitwist/apps/stmanage/stbuild/main.d
+++ src/semitwist/apps/stmanage/stbuild/main.d
@@ -15,43 +15,15 @@ This has been tested to work with DMD 2.048
 //TODO: Handle DMD patched for -ww
 //      Use STBUILD_OPTS env var: STBUILD_OPTS=dmdpatch_ww;whatever...
 //TODO: Disallow crazy characters in target names
-//TODO: $(proj), $(mode), $(#), $proj, $#, $$, etc.
 //TODO? Use Goldie to parse .conf file
 //TODO: Get obj dir from conf switches instead of hardcoding to "obj/target/mode"
-//TODO: Translate extraArgs between rebuild/xfbuild
+//TODO: Translate extraArgs between rebuild/xfbuild/rdmd
 //TODO: When using xfbuild, make sure root filenames end in ".d" (ie "src/main.d" instead of "src/main")
 //TODO: Fix: 'stbuild.conf' errors get displayed before header message.
-//TODO: Fix: Exception when copying .map if old .map already exists at new location.
 
 module semitwist.apps.stmanage.stbuild.main;
 
 import semitwist.cmd.all;
-
-// Needed for tango.sys.Process workaround in build() below
-/+version(Windows) {}
-else
-{
-	import tango.stdc.posix.unistd;
-	import tango.stdc.posix.sys.wait;
-	import tango.stdc.stdlib;
-	import tango.stdc.string;
-	import tango.stdc.stringz;
-    import tango.stdc.errno;
-
-	//This stuff is borrowed from tango/sys/Process.d
-	version(darwin)
-    {
-        extern (C) char*** _NSGetEnviron();
-        private char** environ;
-        
-        static this ()
-        {
-            environ = *_NSGetEnviron();
-        }
-    }
-    extern (C) extern char** environ;
-    import tango.stdc.posix.stdlib;
-}+/
 
 import semitwist.apps.stmanage.stbuild.cmdArgs;
 import semitwist.apps.stmanage.stbuild.conf;
@@ -69,10 +41,6 @@ Conf conf;
 
 void moveMapFiles(string subDir=".")
 {
-//TODO***
-//	foreach(VfsFile mapFile; cmd.dir.self.catalog("*.map"))
-//		cmd.dir.folder("obj/"~subDir).open.file(mapFile.name).move(mapFile);
-
 	foreach(string name; dirEntries(".", SpanMode.shallow))
 	{
 		if(name.fnmatch("*.map"))
@@ -149,87 +117,7 @@ int build(string target, string mode, bool verbose)
 		cmdArgs.extraArgs;
 	
 	if(cmdArgs.showCmd) cmd.echo(cmdLine);
-	//TODO***
-	//version(Windows)
-		ret = system(cmdLine);
-	//else
-	//{
-/+		// On Tango 0.99.9, tango.sys.Process, and therefore cmd.exec, hangs
-		// on non-Windows due to Tango Bug #1859, so this is a quick-n-dirty
-		// workaround.
-		
-		string[] args;
-		//mixin(traceVal!("cmdLine"));
-		foreach(string arg; quotes(cmdLine, " "))
-		{
-			//writefln(":%s", arg);
-			
-			if(arg == "")
-				continue;
-				
-			// UTF-safe because '"' is a single code-unit in any UTF.
-			if(arg[0] == '"')
-				arg = arg[1..$];
-			if(arg[$-1] == '"')
-				arg = arg[0..$-1];
-			
-			args ~= arg;
-		}
-		
-		string execFile = args[0];
-		//char* execFileZ = toStringz(execFile.dup);
-		char*[] argsZ = [];
-		foreach(string arg; args)
-			argsZ ~= toStringz(arg.dup);
-		argsZ ~= null;
-
-		auto pid = fork();
-		if(pid < 0)
-			throw new ProcessException("Failed to fork new process");
-
-		if(pid != 0)
-		{
-			// Parent process
-			int status;
-			waitpid(pid, &status, 0);
-		}
-		else
-		{
-			// Child process
-			
-			// This block is borrowed from tango/sys/Process.d
-            int rc = -1;
-            char* str;
-            if (!contains(execFile, FileConst.PathSeparatorChar) &&
-                (str = getenv("PATH")) !is null)
-            {
-                char[][] pathList = delimit(str[0 .. strlen(str)], ":");
-
-                foreach (path; pathList)
-                {
-                    if (path[path.length - 1] != FileConst.PathSeparatorChar)
-                    {
-                        path ~= FileConst.PathSeparatorChar;
-                    }
-
-                    path ~= execFile;
-                    path ~= '\0';
-
-                    rc = execve(path.ptr, argsZ.ptr, environ);
-                    // If the process execution failed because of an error
-                    // other than ENOENT (No such file or directory) we
-                    // abort the loop.
-                    if (rc == -1 && errno != ENOENT)
-                    {
-                        break;
-                    }
-                }
-            }
-			
-			throw new ProcessException("Program '"~execFile~"' not found");
-		}+/
-		
-	//}
+	ret = system(cmdLine);
 	moveMapFiles(target~"/"~mode);
 	
 	return ret;
@@ -242,22 +130,6 @@ int clean(string target, string mode, bool verbose)
 
 	if(verbose)
 		cmd.echo("Cleaning %s %s...".format(target, mode));
-
-//TODO***
-/+	VfsFolders objDir;
-	try
-		objDir = cmd.dir.folder("obj/"~target~"/"~mode).open.tree;
-	catch(Exception e) // No directory to clean
-		return 0;
-	
-	foreach(VfsFile file; objDir.catalog("*.map"))
-		file.remove();
-	
-	foreach(VfsFile file; objDir.catalog("deps"))
-		file.remove();
-	
-	foreach(VfsFile file; objDir.catalog("*"~objExt))
-		file.remove();+/
 
 	auto objDir = "obj/"~target~"/"~mode; 
 	if(exists(objDir))
@@ -280,4 +152,3 @@ int main(string[] args)
 
 	return process(cmdArgs.target, cmdArgs.mode, !cmdArgs.quiet);
 }
-
