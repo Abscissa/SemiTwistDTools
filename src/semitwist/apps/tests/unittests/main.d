12a5a12a6aba0068cfe94ca766da51b040d66d77
Abscissa256 <Abscissa256>
2011-03-05 08:35:30 +0000

    (NB) unittests: Release build of 'unittests' app just shows a message saying to run the debug version instead.
    git-svn-id: file:///home/nick/dev/d/proj/svn-to-git-scrap/svn-dump/filtered-repos/SemiTwistDTools/trunk@187 183b819f-4d41-4c81-9374-59e59addc796

--- src/semitwist/apps/tests/unittests/main.d
+++ src/semitwist/apps/tests/unittests/main.d
@@ -15,7 +15,18 @@ module semitwist.apps.tests.unittests.main;
 
 import semitwist.cmd.all;
 
+version(unittest) {} else import std.stdio;
+
 void main()
 {
 	flushAsserts();
+	
+	version(unittest) {}
+	else
+	{
+		writeln("This is the release build of the SemiTwistDTools unittests.");
+		writeln("Naturally, it doesn't do anything.");
+		writeln();
+		writeln("Run 'semitwist-unittests-debug' instead.");
+	}
 }
