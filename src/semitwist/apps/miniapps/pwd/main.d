2fbd33cb5301b3ac3f7d2d0d57299a2654658903
Abscissa256 <Abscissa256>
2011-03-05 08:23:48 +0000

    (B) - seterrorlevel: Changed behavior on invalid value. - pwd: Added new miniapp 'pwd' (Print Working Directory).
    git-svn-id: file:///home/nick/dev/d/proj/svn-to-git-scrap/svn-dump/filtered-repos/SemiTwistDTools/trunk@186 183b819f-4d41-4c81-9374-59e59addc796

---
+++ src/semitwist/apps/miniapps/pwd/main.d
@@ -0,0 +1,23 @@
+// SemiTwist D Tools
+// MiniApps: pwd (Print Working Directory)
+// Written in the D programming language.
+
+/++
+Author:
+$(WEB www.semitwist.com, Nick Sabalausky)
+
+AFAIK, Windows doesn't have a 'pwd' command.
+So this might occasionally be useful.
+
+This has been tested to work with DMD 2.050
++/
+
+module semitwist.apps.miniapps.pwd.main;
+
+import std.path;
+import std.stdio;
+
+void main()
+{
+	writeln(rel2abs("."));
+}
--- src/semitwist/apps/miniapps/seterrorlevel/main.d
+++ src/semitwist/apps/miniapps/seterrorlevel/main.d
@@ -6,6 +6,12 @@
 Author:
 $(WEB www.semitwist.com, Nick Sabalausky)
 
+Sets the errorlevel to any value. Could be useful in Windows batch scripts.
+
+Calling 'seterrorlevel' by itself will set the errorlevel to 0.
+Calling 'seterrorlevel' with an invalid value will result in an error message
+and set the errorlevel to 1.
+
 This has been tested to work with DMD 2.049/2.050
 +/
 
@@ -18,8 +24,5 @@ int main(string[] args)
 	if(args.length < 2)
 		return 0;
 	
-	try
-		return to!int(args[1]);
-	catch(Throwable e)
-		return 0;
+	return to!int(args[1]);
 }
