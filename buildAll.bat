4029286d672ba8425573a6475a9aa67deb1a5a00
Nick Sabalausky <Abscissa>
2012-04-14 21:10:17 -0400

    buildAll: Fixed: Last commit broke it.

--- buildAll.bat
+++ buildAll.bat
@@ -1,7 +1,7 @@
 @echo off
 call scripts\bootstrap_stbuild.bat
-bin\semitwist-stbuild-bootstrap all all -clean
-bin\semitwist-stbuild-bootstrap all all -tool:rdmd
+bin\semitwist-stbuild-bootstrap all all --clean
+bin\semitwist-stbuild-bootstrap all all --tool=rdmd
 
 del /Q bin\semitwist-stbuild-bootstrap.exe 2> _junk_.junk
 del /Q _junk_.junk
